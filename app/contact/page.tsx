124303fdf0bc00ac001b9d710473f2311867c6bd
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2025-11-27 18:38:54 +0600

    Update contact page layout for better responsiveness
    Replaced flex-1 classes with md:w-1/3 and md:w-2/3 to improve the layout on medium and larger screens, ensuring better control over the width of the contact information and form sections.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   return (
     <div className="container mx-auto p-4 lg:mt-15 mt-10">
       <div className="flex flex-col md:flex-row gap-8 mt-8">
-        <div className="flex-1 flex flex-col gap-5">
+        <div className="md:w-1/3 w-full flex flex-col gap-5">
           <Card className="bg-[#edeef0] dark:bg-card p-6 rounded-lg shadow-md h-1/2">
             <h2 className="text-xl font-semibold mb-1">Our Location</h2>
             <div className="flex items-start gap-3">
@@ -38,7 +38,7 @@ export default function Contact() {
           </Card>
         </div>
 
-        <div className="flex-1 flex">
+        <div className="md:w-2/3 w-full">
           <Card className="bg-[#edeef0] dark:bg-card p-6 rounded-lg shadow-md w-full h-auto flex flex-col justify-between">
             <h2 className="text-2xl font-bold text-center mt-5">
               Get in Touch
