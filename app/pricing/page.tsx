625965f1630d5c6916d64de62f307491bbe8a26b
Ashikur Rahman Bhuiyan <ashikurrb85@gmail.com>
2025-12-26 18:22:09 +0600

    Enhance pricing page with custom quote and contact sections
    Added a 'Request a custom quote' section and a call-to-action area with contact details and an email input for quote requests. Improved layout responsiveness and updated the pricing toggle UI for better user experience.

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -12,9 +12,11 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
+import { ArrowRight } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 type PricingPlan = {
-    id: string;
+  id: string;
   title: string;
   description: string;
   highlight?: boolean;
@@ -39,7 +41,7 @@ type PricingPlan = {
     value?: string;
     highlight?: boolean;
   }[];
-}
+};
 
 export const pricingPlans: PricingPlan[] = [
   {
@@ -152,8 +154,9 @@ export default function Pricing() {
   const [yearly, setYearly] = useState(false);
 
   return (
-    <section className="py-10 px-6 lg:px-24">
-      <div className="max-w-7xl mx-auto text-center">
+    <div>
+      {/* Main Pricing */}
+      <section className="py-10 px-6 lg:px-24 max-w-7xl mx-auto text-center">
         <h2 className="text-4xl font-extrabold text-white">
           Pricing for every stage
         </h2>
@@ -163,23 +166,23 @@ export default function Pricing() {
         </p>
 
         {/* TOGGLE */}
-        <div className="mt-8 flex items-center justify-center gap-4">
-          <span className="text-sm text-zinc-300">Monthly</span>
-
-          <Switch
-            checked={yearly}
-            onCheckedChange={setYearly}
-            className="
+        <div className="mt-8 flex flex-col lg:flex-row items-center justify-center gap-4">
+          <div className="flex gap-5 justify-center items-center">
+            <span className="text-sm text-zinc-300">Monthly</span>
+            <Switch
+              checked={yearly}
+              onCheckedChange={setYearly}
+              className="
               scale-150
               data-[state=unchecked]:bg-zinc-700
               data-[state=checked]:bg-red-600
               [&>span]:bg-white
               transition-all
+              cursor-pointer
             "
-          />
-
-          <span className="text-sm text-zinc-300">Yearly</span>
-
+            />
+            <span className="text-sm text-zinc-300">Yearly</span>
+          </div>
           <span
             className={`
               text-sm text-red-600 font-bold
@@ -194,7 +197,7 @@ export default function Pricing() {
         </div>
 
         {/* CARDS */}
-        <div className="mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-4">
+        <div className="lg:mt-10 mt-3 grid gap-6 md:grid-cols-2 lg:grid-cols-4">
           {pricingPlans.map((plan) => {
             const price = yearly ? plan.pricing.yearly : plan.pricing.monthly;
 
@@ -248,7 +251,10 @@ export default function Pricing() {
                   </Button>
                   <ul className="space-y-3 text-sm">
                     {plan.features.map((f, i) => (
-                      <li key={i} className="flex justify-between border-b border-zinc-800 pb-3">
+                      <li
+                        key={i}
+                        className="flex justify-between border-b border-zinc-800 pb-3"
+                      >
                         <span className="text-muted-foreground">{f.label}</span>
                         <span className="text-white font-bold"> {f.value}</span>
                       </li>
@@ -259,7 +265,105 @@ export default function Pricing() {
             );
           })}
         </div>
-      </div>
-    </section>
+      </section>
+
+      {/* custom Qoute */}
+      <section className="px-6 lg:px-24 my-10">
+        <div className="flex flex-col lg:flex-row text-center lg:text-start justify-center lg:justify-between items-center p-10 border-[#242426] border-2 rounded-xl bg-gradient-to-r from-[#18181b] to-[#090102] gap-5">
+          <div>
+            <h3 className="text-2xl font-extrabold text-white">
+              Coudn't meet your needs?{" "}
+            </h3>
+            <p className="mt-3 text-muted-foreground">
+              We offer tailored packages for large scale operations.
+            </p>
+          </div>
+          <Button className="font-bold rounded-3xl bg-white text-primary hover:bg-white cursor-pointer py-5">
+            <Link
+              href="/contact"
+              className="flex flex-row items-center gap-2 justify-center"
+            >
+              <span className="hover:text-red-700">
+                {" "}
+                Request a custom quote
+              </span>
+              <span className="hover:text-white">
+                <ArrowRight />
+              </span>
+            </Link>
+          </Button>
+        </div>
+      </section>
+
+      {/* Call Details */}
+      <section className="bg-[#0c0c0d] py-[48px] mt-20 border-b-3 border-transparent hover:border-red-700 transition-all duration-300">
+        <div className="flex flex-col lg:flex-row px-4 sm:px-8 lg:px-[160px] gap-10 lg:gap-0 justify-center lg:justify-between items-center">
+          <div className="space-y-6 text-center lg:text-left">
+            <div className="space-y-2">
+              <h4 className="text-red-600 uppercase font-bold text-sm tracking-wide">
+                Call Now
+              </h4>
+              <h1 className="font-bold text-red-600 text-3xl">
+                +880 1711-205200
+              </h1>
+            </div>
+
+            <div>
+              <h1 className="text-white font-bold text-2xl leading-tight">
+                Get One Step Digital Solution Under One Roof
+              </h1>
+              <p className="text-muted-foreground mt-3">
+                Web Development, Creative Writing, Digital Marketing, Graphics &
+                Video Editing, & so on.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full flex justify-center lg:justify-end">
+            <div className="relative w-full sm:w-[420px]">
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                className="
+                w-full
+                border-[#3f3f46]
+                bg-[#070707]
+                text-white
+                placeholder:text-gray-500
+                text-lg
+                rounded-full
+                pl-7 pr-[150px]
+                py-7
+                focus:border-red-600
+                focus:ring-2 focus:ring-red-600/30
+                transition-all
+              "
+              />
+
+              <Button
+                className="
+                absolute
+                right-2
+                top-1/2
+                -translate-y-1/2
+                bg-[#e7000b]
+                text-white
+                text-md
+                rounded-full
+                px-6
+                py-5
+                hover:bg-[#ef4444]
+                active:scale-[0.98]
+                transition-all
+                cursor-pointer
+              "
+              >
+                Get Quote
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
